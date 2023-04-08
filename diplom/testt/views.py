a14205db9e20638a0f94993798b01c8a897d9db0
Lttude <62471584+Morrisonn@users.noreply.github.com>
2023-04-08 15:35:59 +0300

    Lesson 6 (Templates)

--- diplom/testt/views.py
+++ diplom/testt/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from .models import *
+
+menu = ["1","2","3"]
 
 def index(request):
-    if(request.GET):
-        print(request.GET)
-    return HttpResponse("СТраница приложения testt")
+    posts = Women.objects.all()
+    return render(request, 'testt/index.html', {'posts' : posts ,'menu': menu, 'title': 'Главная'})
+
+def about(request):
+    return render(request, 'testt/about.html', {'menu': menu, 'title': 'О сайте'})
 
 def categories(request, csid):
     return HttpResponse(f"<h1>СТраница приложения testt<h1><p>{csid}</p>")
